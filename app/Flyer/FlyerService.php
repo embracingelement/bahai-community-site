e0de1f84e3c4711b1bd5ce1a7f3c0f2e27b47989
dominic_scimeca <dominic.scimeca@surveysampling.com>
2017-09-19 21:23:23 -0700

    updating flyers to show and hide based off of date. updated original flyer to include the date

--- app/Flyer/FlyerService.php
+++ app/Flyer/FlyerService.php
@@ -19,40 +19,98 @@ class FlyerService {
 
 
     /**
-     * @param Flyer[] $flyers
      * @return Flyer[]
      */
     function getFlyers(){
         $flyers = [];
 
-        $files_in_root = $this->getFileInDir($this->flyerDir);
+        $files_in_root = $this->getFilesInDir($this->flyerDir);
 
         foreach($files_in_root as $file){
 
-            if(is_dir($file)){
-              $files_in_dir = $this->getFileInDir($file);
-              $new_flyer = false;
+            if(is_dir($file) && $this->dirShouldBeProcessed($file) && $this->flyerIsInTheFuture($file) ){
 
-              if(sizeof($files_in_dir) == 1){
-                  $new_flyer = new Flyer($this->getPathRelativeToPublic($files_in_dir[0]));
-              }elseif (sizeof($files_in_dir) == 2){
-                  if(false === strpos($files_in_dir[0],"thumbnail")){
-                      $new_flyer = new Flyer($this->getPathRelativeToPublic($files_in_dir[0]), $this->getPathRelativeToPublic($files_in_dir[1]));
-                  }else{
-                      $new_flyer = new Flyer($this->getPathRelativeToPublic($files_in_dir[1]), $this->getPathRelativeToPublic($files_in_dir[0]));
-                  }
-              }
+              $files_in_dir = $this->getFilesInDir($file);
+              $large_and_thumbnail = $this->getLargeAndThumbnail($files_in_dir);
 
-              if(false != $new_flyer){
-                  array_push($flyers, $new_flyer);
-              }
+              array_push($flyers, new Flyer(
+                  $this->getPathRelativeToPublic($large_and_thumbnail['large']),
+                  $this->getPathRelativeToPublic($large_and_thumbnail['thumbnail'])
+              ));
             }
         }
 
         return $flyers;
     }
 
-    private function getFileInDir($dir){
+    private function dirShouldBeProcessed($dir){
+        $files_in_dir = $this->getFilesInDir($dir);
+        return (sizeof($files_in_dir) == 1 || sizeof($files_in_dir) == 2) && $this->getFlyerDate($dir);
+    }
+
+
+    private function getLargeAndThumbnail($files_in_dir){
+        $large_and_thumbnail = [];
+        if(sizeof($files_in_dir) == 1){
+            $large_and_thumbnail['large'] = $files_in_dir[0];
+            $large_and_thumbnail['thumbnail'] = $files_in_dir[0];
+        }elseif (sizeof($files_in_dir) == 2){
+            if(false === strpos($files_in_dir[0],"thumbnail")){
+                $large_and_thumbnail['large'] = $files_in_dir[0];
+                $large_and_thumbnail['thumbnail'] = $files_in_dir[1];
+            }else{
+                $large_and_thumbnail['large'] = $files_in_dir[1];
+                $large_and_thumbnail['thumbnail'] = $files_in_dir[0];
+            }
+        }
+
+        return $large_and_thumbnail;
+    }
+
+    /**
+     * @param $filename
+     * @return bool
+     */
+    private function flyerIsInTheFuture($filename){
+        $flyer_date = $this->getFlyerDate($filename);
+        return $this->isDateInTheFuture($flyer_date);
+    }
+
+    /**
+     * @param $date_array
+     * @return bool
+     */
+    private function isDateInTheFuture($date_array){
+        $current_date_array = getdate();
+
+        if($date_array['year'] > $current_date_array['year']){
+            return true;
+        }else if($date_array['year'] < $current_date_array['year']){
+            return false;
+        }
+
+        if($date_array['month'] > $current_date_array['mon']){
+            return true;
+        }else if($date_array['month'] < $current_date_array['mon']){
+            return false;
+        }
+
+        if($date_array['day'] > $current_date_array['mday']){
+            return true;
+        }else if($date_array['day'] < $current_date_array['mday']){
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private function getFlyerDate($filename){
+        $date_array = explode("::",array_pop(explode("/",$filename)));
+        return !empty($date_array[1]) ? date_parse($date_array[0]) : false;
+    }
+
+    private function getFilesInDir($dir){
         $filenames = array_diff(scandir($dir), array('..', '.'));
 
         $files = [];
